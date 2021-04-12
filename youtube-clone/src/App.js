8fd951a0ee5d23a0d959ffe9a65da87787aee066
ehtishasmasghar13 <ehtishamasghar13@gmai.com>
2021-04-12 04:16:59 -0700

    header bar design done.

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,10 +1,14 @@
-
+import React from "react";
 import './App.css';
+import  Header from "./Header";
+
 
 function App() {
   return (
     <div className="App">
-        <h1>hellow malik</h1>
+      <Header />
+      {/* <Sidebar/> */}
+
     </div>
   );
 }
---
+++ youtube-clone/src/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Header.css';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationIcon from '@material-ui/icons/Notifications';
+import Avatar from "@material-ui/core/Avatar";
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <MenuIcon />
+                <img className="header__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/512px-YouTube_Logo_2017.svg.png"/>
+            </div>
+            <div className="header__input">
+                <input placeholder="Search" type="text" />
+                <SearchIcon className="header_inputButton" />   
+            </div>
+            <div className="header__icons">
+                <VideoCallIcon className="header__icon" />
+                <AppsIcon className="header__icon" />
+                <NotificationIcon className="header__icon"/>
+                <Avatar src="https://avatars.githubusercontent.com/u/57295751?v=4" alt="Malik" />
+            </div>
+            
+
+
+
+        
+        </div>
+    )
+}
+
+export default Header
